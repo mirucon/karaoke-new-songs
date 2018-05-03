6747d9d8ff1146ffab4ad4255114c2e4c0bbf96f
mirucon <i@miruc.co>
2018-05-03 22:18:19 +0900

    Add: Open Graph tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,18 +4,32 @@ module.exports = {
   /*
   ** Headers of the page
   */
+  htmlAttrs: {
+    lang: 'ja',
+    prefix: 'http://ogp.me/ns/website#'
+  },
   head: {
-    title: 'カラオケ週間配信曲クイックビューワー',
+    title: 'カラオケ最新曲クイックビューワー',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'DAMとJOYSOUNDの最新配信曲を同時表示 + 横断検索 !' }
+      { hid: 'description', name: 'description', content: 'DAMとJOYSOUNDの最新配信曲を同時表示 + 横断検索 !' },
+      { property: 'og:description', content: 'DAMとJOYSOUNDの最新配信曲を同時表示 + 横断検索 !' },
+      { property: 'og:site_name', content: 'カラオケ最新曲クイックビューワー' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://karaokenewsongs.com/' },
+      { property: 'og:image', content: '/favicon-192x192.png' },
+      { property: 'og:locale', content: 'ja' },
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:domain', content: 'https://karaokenewsongs.com/' },
+      { name: 'twitter:site', content: '@mirucons' },
+      { name: 'twitter:creator', content: '@mirucons' }
     ],
     link: [
-      { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
-      { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
-      { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
-      { rel: 'icon', type: 'image/png', href: '/favicon-192x192.png', sizes: '192x192' },
+      { rel: 'icon', href: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { rel: 'icon', href: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { rel: 'icon', href: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { rel: 'icon', href: '/favicon-192x192.png', sizes: '192x192', type: 'image/png' },
       { rel: 'apple-touch-icon', href: '/favicon-120x120.png', sizes: '120x120' },
       { rel: 'apple-touch-icon', href: '/favicon-152x152.png', sizes: '152x152' },
       { rel: 'apple-touch-icon', href: '/favicon-167x167.png', sizes: '167x167' },
@@ -55,13 +69,20 @@ module.exports = {
     }
   },
   modules: [
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-56308747-9'
+    }],
+    ['@nuxtjs/google-adsense', {
+      id: 'ca-pub-6450842193166394',
+      test: true
+    }]
   ],
   workbox: {
     // dev: true
   },
   manifest: {
-    name: 'カラオケ新着週間配信曲',
+    name: 'カラオケ最新曲クイックビューワー',
     lang: 'ja'
   }
 }
